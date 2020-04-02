84d451bb6e348e7c9920db5927352da2c0bac77b
dgoat416 <55709445+dgoat416@users.noreply.github.com>
2020-04-01 20:08:52 -0700

    Initial Commit (Just finished this)
    - Quick Sort Algorithm
    * and one that works with generic types

---
+++ QuickSort.java
@@ -0,0 +1,181 @@
+import java.util.Random;
+
+/**
+ * Class to hold and test the quick sort algorithm
+ * Quick sort is an efficient in place sorting algorithm that
+ * performs 2-3x faster than merge sort and heapsort if implemented well.
+ * 
+ * Quick sort on averages takes O(n log n) comparisons to sort a list of size n
+ * but O(n ^2) in the worst case
+ * 
+ * Quick sort is a divide and conquer algorithm that divides an array of size n into 
+ * subarrays of size n / 2 and sorts them recursively
+ * STEPS:
+ * 1. Choose a pivot to split the array around 
+ * 2. Partition the array around the pivot to create two new arrays 
+ * 		(reorder the array so that every element after the pivot is greater than 
+ * 		 the pivot and every element before the the pivot is less than the pivot)
+ * 3. Recursively do this until everything is sorted
+ * 
+ * The beautiful thing about quick sort is it is a comparison sort algorithm
+ * so as long as there is a less-than relation within the data being sorted 
+ * it can be used.
+ * @author Deron Washington II
+ * Last Updated: 3/27/20
+ *
+ */
+public class QuickSort
+{
+
+	/**
+	 * Method to swap the elements at the indexes
+	 * specified by the parameters
+	 * @param array   = array to swap the elements
+	 * @param index1 = index of first element to be swapped
+	 * @param index2 = index of second element to be swapped
+	 */
+	public static void swap(int[] array, int index1, int index2)
+	{
+		if (index1 != index2)
+		{
+			int temp = array[index1];
+			array[index1] = array[index2];
+			array[index2] = temp;
+		}
+	}
+
+	/**
+	 * Method to partition the elements in array around the pivot
+	 * so that every entry to the left of the pivot is less than the pivot
+	 * and so that every entry to the right of the pivot is greater than
+	 * the pivot
+	 * @param array  = array to partition
+	 * @param pivot   = value of the pivot
+	 * @param sIndex = starting Index to partition around
+	 * @param eIndex = ending Index to partition around (location of pivot)
+	 * @return 
+	 * 				= the index of the pivot
+	 */
+	public static int partition(int[] array, int pivot, int sIndex, int eIndex)
+	{
+		int currStart = sIndex;
+		int currEnd = eIndex - 1;
+		boolean done = false;
+
+		
+		while (done == false)
+		{
+			// if the left reference is greater than or equal to the
+			// right reference then left reference passed the right reference
+			// so you need to insert the pivot at the left reference which is now
+			// on the right (direction) side of the pivot so everything is now in
+			// the correct partitions
+			if (currStart > currEnd)
+			{
+				if (array[currStart] > array[eIndex])
+					swap(array, currStart, eIndex);
+				done = true;
+			}
+
+			// if left reference's value is less than the pivot value it is in the correct 
+			// partition so move the reference to the next spot
+			else if (array[currStart] < pivot)
+				currStart++;
+
+			// if right reference's value is greater than the pivot value it is in the 
+			// correct partition so move the reference to the next spot
+			else if (array[currEnd] > pivot)
+				currEnd--;
+
+			// if right reference's value is less than pivot value and the   
+			// left reference's value is greater than pivot value then both 
+			// reference values are in the wrong partitions so swap them
+			else if (array[currStart] > pivot && array[currEnd] < pivot)
+				swap(array, currStart, currEnd);
+
+		}
+
+		// return the index of the pivot
+		return currStart;
+	}
+
+	/**
+	 * Method to create random pivot between starting Index in 
+	 * the array and the ending Index
+	 * @param array   = array to partition
+	 * @param sIndex = index to start looking for a pivot to partition around
+	 * @param eIndex = index to stop looking for a pivot to partition around
+	 */
+	public static int randomPivot(int[] array, int sIndex, int eIndex)
+	{
+		// choose random pivot
+		int pivotIndex = -1;
+		int size = (eIndex - sIndex) + 1;
+		Random rand = new Random();
+
+		// random pivot between sIndex and eIndex
+		pivotIndex = rand.nextInt(size) + sIndex;
+
+		// swap the pivot and the last element
+		swap(array, pivotIndex, eIndex);
+
+		// partition the array around the pivot & return the pivot's index
+		return partition(array, array[eIndex], sIndex, eIndex);
+
+	}
+
+	/**
+	 * Method to partition around the last element
+	 * @param array   = array to partition
+	 * @param sIndex = index to start looking for a pivot to partition around
+	 * @param eIndex = index to stop looking for a pivot to partition around
+	 */
+	public static int lastElementPivot(int[] array, int sIndex, int eIndex)
+	{
+		// choose last element as pivot
+		int pivotIndex = eIndex;
+
+		// partition the array around the pivot & return the pivot's index
+		return partition(array, array[eIndex], sIndex, eIndex);
+	}
+
+	/**
+	 * Method to implement the quick sort algorithm
+	 * (this version uses a random partition to minimize the affects 
+	 * of the worst case of quick sort)
+	 * @param array
+	 * @param sIndex
+	 * @param eIndex
+	 */
+	public static void quickSort(int[] array, int sIndex, int eIndex)
+	{
+		if (sIndex < eIndex)
+		{
+			// partition
+			int pivotIndex =  lastElementPivot(array, sIndex, eIndex);
+									//randomPartition(array, sIndex, eIndex);
+			// call quick sort on the list smaller than the pivot 
+			quickSort(array, sIndex, pivotIndex - 1);
+
+			// call quick sort on the list larger than the pivot
+			quickSort(array, pivotIndex + 1, eIndex);
+		}
+	}
+
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int[] arr = {0,9,8, 45, 7, 2, 33, 44, 17};
+
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + "\n");
+
+		quickSort(arr, 0, arr.length - 1);
+
+		for (int j = 0; j < arr.length; j++)
+			System.out.println(arr[j]);
+
+	}
+
+}
---
+++ QuickSortGeneric.java
@@ -0,0 +1,189 @@
+import java.util.Random;
+/**
+ * Class that houses and tests the quick sort algorithm that works for any
+ * comparable objects
+ * @author Deron Washington II
+ * Last Updated: 4/1/20
+ * @param <T> = an object that can be represented using a less than relation and 
+ * 						     implements Comparable
+ */
+public class QuickSortGeneric<T extends Comparable<T>>
+{
+
+	/**
+	 * Method to swap the values located at index1 and index2 in the array parameter
+	 * @param <T>       = an object that can be represented using a less than relation and 
+	 * 								  implements Comparable
+	 * @param arrayObj  = an array of objects that implements Comparable
+	 * @param index1     = 1st index to swap in the array
+	 * @param index2		= 2nd index to swap in the array
+	 */
+	public static <T extends Comparable<T>> void swap(T[] arrayObj, int index1, int index2)
+	{
+		T temp = arrayObj[index1];
+		arrayObj[index1] = arrayObj[index2];
+		arrayObj[index2] = temp;
+	}
+	
+	/**
+	 * Method to create random pivot between starting Index in 
+	 * the array and the ending Index
+	 * @param <T>    = an object that can be represented using a less than relation and 
+	 * 							    implements Comparable
+	 * @param array   = array to partition
+	 * @param sIndex = index to start looking for a pivot to partition around
+	 * @param eIndex = index to stop looking for a pivot to partition around
+	 * @return
+	 * 				= the index of the pivot
+	 */
+	public static <T extends Comparable<T>> int randomPivot(T[] arrayObj, int sIndex, int eIndex)
+	{
+		Random rand = new Random();
+		int size = (eIndex - sIndex) + 1;
+		int pivotIndex = rand.nextInt(size) + sIndex;
+		
+		// swap the value of the pivot with the value of the last element
+		swap(arrayObj, pivotIndex, eIndex);
+		
+		return partition(arrayObj, sIndex, eIndex);
+	}
+	
+	
+	/**
+	 * Method to partition around the last element
+	 * @param <T>    = an object that can be represented using a less than relation and 
+	 * 							    implements Comparable
+	 * @param array   = array to partition
+	 * @param sIndex = index to start looking for a pivot to partition around
+	 * @param eIndex = index to stop looking for a pivot to partition around
+	 */
+	public static <T extends Comparable<T>> int lastElementPivot(T[] arrayObj, int sIndex, int eIndex)
+	{
+		// choose last element as pivot
+		int pivotIndex = eIndex;
+
+		// partition the array around the pivot & return the pivot's index
+		return partition(arrayObj, sIndex, eIndex);
+	}
+	
+	/**
+	 * Method to partition the arrayObj parameter around the pivot 
+	* @param <T>        = an object that can be represented using a less than relation and 
+	 * 								   implements Comparable
+	 * @param arrayObj  = an array of objects that implements Comparable
+	 * @param sIndex     = starting Index of the array
+	 * @param eIndex		= ending Index of the array
+	 * @return
+	 * 				= the index of the pivot
+	 */
+	public static <T extends Comparable<T>> int partition(T[] arrayObj, int sIndex, int eIndex)
+	{
+		int currStart = sIndex;
+		int currEnd = eIndex - 1;
+		T pivot = arrayObj[eIndex];
+		
+		while(currStart <= currEnd)
+		{
+			if (arrayObj[currStart].compareTo(pivot) < 0)
+				currStart++;
+			
+			else if (arrayObj[currEnd].compareTo(pivot) > 0)
+				currEnd--;
+			
+			else if (arrayObj[currStart].compareTo(pivot) > 0
+					&& arrayObj[currEnd].compareTo(pivot) < 0)
+					swap(arrayObj, currStart, currEnd);
+		}
+		
+		// place pivot in correct position
+		swap(arrayObj, currStart, eIndex);
+		
+		return currStart;
+	}
+	
+	/**
+	 * Quick sort algorithm to be used for any objects that
+	 * can be compared
+	 * @param <T>       = an object that can be represented using a less than relation and 
+	 * 								   implements Comparable
+	 * @param arrayObj  = an array of objects that implements Comparable
+	 * @param sIndex     = starting Index of the array
+	 * @param eIndex		= ending Index of the array
+	 */
+	public static <T extends Comparable<T>> void quickSort(T[] arrayObj, int sIndex, int eIndex)
+	{
+		if (sIndex < eIndex)
+		{
+			// find the pivot
+			int pivotIndex = randomPivot(arrayObj, sIndex, eIndex);
+			
+			// sort the left side of the pivot
+			quickSort(arrayObj, sIndex, pivotIndex - 1);
+			
+			// sort the right side of the pivot
+			quickSort(arrayObj, pivotIndex + 1, eIndex);
+		}
+	}
+	
+	/**
+	 * Method to output the array to the console 
+	 * @param <T>       = any data type
+	 * @param arrayObj = an array of <T> objects to print
+	 */
+	public static <T> void printArray(T[] arrayObj)
+	{
+		System.out.println("Printing array: ");
+		
+		for (int i = 0; i < arrayObj.length; i++)
+			System.out.print(arrayObj[i] + " ");
+		
+		System.out.printf("%n%n");
+	}
+	
+	/**
+	 * Method to output the array to the console 
+	 * @param <T>       = any data type
+	 * @param arrayObj = an array of <T> objects to print
+	 */
+	public static <T> void printArray(T[] arrayObj, String message)
+	{
+		System.out.println("Printing array " + message + ": ");
+		
+		for (int i = 0; i < arrayObj.length; i++)
+			System.out.print(arrayObj[i] + " ");
+		
+		System.out.printf("%n%n");
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Integer[] intArr = {0,9,8, 45, 7, 2, 33, 44, 17};
+		Character[] charArr = {'z', 'c', 'x', 'd', 'e', 'a', 'f', 't', 'b'};
+		String[] stringArr = {"temp", "Temp", "abc", "def", "Dgoat", "otis", "aaaaaaz", "aaz"};
+		
+		
+		// generic quick sort testing 
+		String beforeMessage = "Before sort ";
+		String afterMessage = "After sort ";
+		
+		// Integer array testing
+		printArray(intArr, beforeMessage);
+		quickSort(intArr, 0, intArr.length - 1);
+		printArray(intArr, afterMessage);
+		
+		// Character array testing
+		printArray(charArr, beforeMessage);
+		quickSort(charArr, 0, charArr.length - 1);
+		printArray(charArr, afterMessage);
+		
+		// String array testing
+		printArray(stringArr, beforeMessage);
+		quickSort(stringArr, 0, stringArr.length - 1);
+		printArray(stringArr, afterMessage);
+		
+		
+	}
+
+}
